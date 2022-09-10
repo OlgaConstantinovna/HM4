11986a60eb30969a608da7369ce92032f1538076
Olga <princessca.olga@icloud.com>
2022-09-10 10:19:10 +0300

    Array

---
+++ AB/Sum/Array/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int[] FillArrayNumber(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i]=new Random().Next(-999,999);
+    }
+return array;
+}
+void WriteArrayPrint(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+       Console.Write(array[i]+ " "); 
+    }
+}
+
+int [] number = FillArrayNumber(new int[8]);
+WriteArrayPrint(number);
